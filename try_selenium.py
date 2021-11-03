ac9dc27d04a2c66122a00241ea019769818c3ef0
autobot123 <52976213+autobot123@users.noreply.github.com>
2021-11-03 23:36:08 +0000

    update to select gpus

--- try_selenium.py
+++ try_selenium.py
@@ -41,9 +41,16 @@ driver = webdriver.Chrome(chromedriver_path, options=chrome_options)
 nvidia_store_url = r'https://store.nvidia.com/en-gb/geforce/store/'
 driver.get(nvidia_store_url)
 sleep_random()
-rtx_3060_ti_tickbox_xpath = '//*[@id="gpu_filter"]/div/div/div/ul/li[6]/div/label'
-gpu_tickbox = get_element_by_xpath(driver, rtx_3060_ti_tickbox_xpath)
-gpu_tickbox.click()
+
+# store as json?
+gpu_list_id = {}
+gpu_list_id['rtx 3060 ti'] = 6
+gpu_list_id['rtx 3070 ti'] = 5
+
+for gpu, list_id in gpu_list_id.items():
+    tickbox_xpath = f'//*[@id="gpu_filter"]/div/div/div/ul/li[{list_id}]/div/label'
+    gpu_tickbox = get_element_by_xpath(driver, tickbox_xpath)
+    gpu_tickbox.click()
 
 nvidia_manufacturer_button_xpath = '//*[@id="manufacturer_filter"]/div/div/div/ul/li[1]/div/label'
 manufacturer_tickbox = get_element_by_xpath(driver, nvidia_manufacturer_button_xpath)
@@ -52,6 +59,7 @@ manufacturer_tickbox.click()
 buy_btns = [get_element_proper_by_class_name(driver, 'featured-buy-link')]
 
 for btn in buy_btns:
+    print(f"button text = {btn.text}")
     if btn.text != 'OUT OF STOCK':
         print(f"gpu in stock")
 
