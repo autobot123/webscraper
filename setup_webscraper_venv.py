558f4c893479dd3a15bc5c7eb3435787bc1e8279
autobot123 <52976213+autobot123@users.noreply.github.com>
2021-11-06 01:43:29 +0000

    add function

--- setup_webscraper_venv.py
+++ setup_webscraper_venv.py
@@ -1,9 +1,16 @@
 import subprocess
 import sys
 
+
+def install_module(module_name):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', module_name])
+    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+
+
+### SCRIPT ###
+
 python_modules = 'selenium,spacy,pytest,pandas,argparse'
 python_modules_list = python_modules.split(",")
 
-for module in python_modules_list:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', module])
-    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+for module_name in python_modules_list:
+    install_module(module_name)
