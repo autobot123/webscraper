b5f44d7b17c8a2c641425991bb5a98bb955e446f
autobot123 <52976213+autobot123@users.noreply.github.com>
2021-11-26 00:24:00 +0000

    add price comparison to tangent ampster

--- webscrape_tangent_ampster.py
+++ webscrape_tangent_ampster.py
@@ -1,4 +1,5 @@
 from webscraper import Webscraper
+import re
 
 def main():
     webscraper = Webscraper(set_headless=True)
@@ -13,9 +14,12 @@ def main():
 
     tangent_ampster_price_xpath = r'//*[@id="product-price-17099"]/span'
     prices = webscraper.get_element_proper_by_class_name(webscraper.driver, 'price')
-    tangent_ampster_price = prices[1]
-    print(f"tangent ampster bt ii price = {tangent_ampster_price.text}")
+    tangent_ampster_price = re.sub("[^0-9]", "", prices[1].text)
 
+    if int(tangent_ampster_price) < 179:
+        print(f"item cheaper than desired price £{tangent_ampster_price}")
+    elif int(tangent_ampster_price) >= 179:
+        print(f"tangent ampster price too high £{tangent_ampster_price}")
 
 if __name__ == "__main__":
     main()
